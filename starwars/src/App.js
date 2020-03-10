7063800d3e67745805aa39c7706d9520c7d4b03a
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-03-10 12:00:04 -0700

    reattempt setup

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { data } from "./data";
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characterArr, setCharacterArr] = useState([]);
+
+  useEffect(() => {
+    //axios isn't allowing network requests right now
+    setCharacterArr(data.results);
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <App />
     </div>
   );
-}
+};
 
 export default App;
